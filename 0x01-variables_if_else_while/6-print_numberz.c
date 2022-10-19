662bd6bbb26f8f21440d90b84db18091095bb430
Shelley_Stephanie <shelley7247@gmail.com>
2022-10-19 12:54:59 -0700

    Bleh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main-Code entry point
+ *Return:0 if no errors,non zero incase pf errors
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
