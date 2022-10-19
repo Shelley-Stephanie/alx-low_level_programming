332cf9d31f1a2a9755338880eb4a02d77c0b66b6
Shelley_Stephanie <shelley7247@gmail.com>
2022-10-19 15:36:55 -0700

    Boo

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main:Entry
+ *Return:Exit
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	if (i == 'e' || i == 'q')
+	continue;
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
