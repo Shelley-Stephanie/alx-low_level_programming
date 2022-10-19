c2f253eaa7b02e2524fcc6273bb60d7a673e54cd
Shelley_Stephanie <shelley7247@gmail.com>
2022-10-19 13:41:58 -0700

    Bleh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *Return:0 if there are no erros and non zero if there are errors
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i = '9'; i++)
+	{
+	putchar(i);
+	if (i == '9')
+	{
+	break;
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
