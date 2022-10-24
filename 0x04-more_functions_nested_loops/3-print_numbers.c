7507d0a7a8a2c4ebf2d7b881ac3fa6ab0e39219f
Shelley_Stephanie <shelley7247@gmail.com>
2022-10-24 08:43:27 -0700

    Bleh

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,12 @@
  **/
 void print_numbers(void)
 {
-char c;
+	char c;
 
-while (c <= '9')
-{
-	_putchar(c + '0');
-	c++;
-}
-_putchar('\n');
+	while (c <= '9')
+	{	
+		_putchar(c + '0');
+		c++
+	}	
+	_putchar('\n');
 }
