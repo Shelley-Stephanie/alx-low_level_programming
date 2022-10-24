5e1e6dd0655f04e5648f0b8d19ec3a8c46445487
Shelley_Stephanie <shelley7247@gmail.com>
2022-10-24 01:19:43 -0700

    Blhe

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_numbers- prints numbers
+ *Return: Values
+ **/
+void print_numbers(void)
+{
+char c;
+
+for (c >= '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+}
