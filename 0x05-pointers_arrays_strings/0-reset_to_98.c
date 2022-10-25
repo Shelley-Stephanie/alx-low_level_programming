467b82fb180635bbf6651f92c3629501baf4a401
Shelley_Stephanie <shelley7247@gmail.com>
2022-10-25 15:48:14 -0700

    Ble

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * reset_to_98 - will do a reset
+ * reset_to_98 - reset
  * @n:a pointer to be allocates
- * Return:O if there are no errors and non zero if there are error
+ * Return: O if there are no errors and non zero if there are error
  **/
-
-void reset_to_0098(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
